7e738c2b81a28a4c3342330b949ec4e7506531ff
Juliet Eyraud <juliet@turing.edu>
2024-04-15 20:44:35 -0500

    add low level caching to API call

--- app/services/ip_location_service.rb
+++ app/services/ip_location_service.rb
@@ -1,7 +1,10 @@
 class IpLocationService
   def self.get_location
-    raw = Faraday.get("https://ipwho.is")
-    response = JSON.parse(raw.body, symbolize_names: true)
-    response[:city]
+    Rails.cache.fetch("user-location", expires_in: 2.hours) do
+      raw = Faraday.get("https://ipwho.is")
+      response = JSON.parse(raw.body, symbolize_names: true)
+      require 'pry'; binding.pry # you'll only hit this when the page loads the first time, or after cache invalidation
+      response[:city]
+    end
   end
 end
